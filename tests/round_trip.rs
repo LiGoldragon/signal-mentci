32f04f44901a9b59a73185213d54811d5d11f7c7
li <li@goldragon.criome.net>
2026-06-26 14:58:56 +0200

    signal-mentci: use nota crate key

--- tests/round_trip.rs
+++ tests/round_trip.rs
@@ -1,4 +1,4 @@
-use nota_next::{NotaDecode, NotaEncode, NotaSource};
+use nota::{NotaDecode, NotaEncode, NotaSource};
 use signal_frame::{
     ExchangeIdentifier, ExchangeLane, LaneSequence, NonEmpty, Reply, RequestPayload, SessionEpoch,
     SubReply,
